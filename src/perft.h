df23af12c3fe888f8b2225f7ec3ec1f822e8ba9d
Adrian Tudev <tudev@chalmers.se>
2026-03-06 09:27:45 +0100

    perft environment setup

--- src/perft.h
+++
@@ -1,6 +0,0 @@
-#ifndef PERFT_H
-#define PERFT_H
-
-#include <string_view>
-
-#endif // PERFT_H
---
+++ tests/perft.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "position.h"
+
+int main() {
+  std::cout << "PERFT tests" << std::endl;
+
+  return 0;
+}
