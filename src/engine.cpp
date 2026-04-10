e8ca8f57df54c17dd9458f99bdbcfb58419e413d
Adrian Tudev <tudev@chalmers.se>
2026-04-10 12:16:25 +0200

    naive negated minimax search algorithm

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,32 +1,75 @@
 #include "engine.h"
+#include "movegen.h"
 
 Engine::Engine() {
-
 }
 
 Score Engine::eval() {
+  return eval(position);
+}
+
+void Engine::set_position(const Position &pos) { position = pos; }
+
+Move Engine::ponder() {
+  Move best_move;
+  Score best_score = -INF;
+  auto all_moves = MoveGen::generate_moves(position);
+
+  if (all_moves.empty()) return Move{};
+
+  for (auto move : all_moves) {
+    auto undo_info = position.do_move(move);
+    Score cur_score = -negamax(position, SEARCH_DEPTH);
+    position.undo_move(undo_info);
+
+    if (cur_score > best_score) {
+      best_move = move;
+      best_score = cur_score;
+    }
+  }
+
+  return best_move;
+}
+
+// Negated Minimax searches for the optimal score in the given position
+Score Engine::negamax(Position pos, uint8_t depth) {
+  if (depth == 0) return eval(pos);
+
+  Score best = -INF;
+  auto all_moves = MoveGen::generate_moves(pos);
+  if (all_moves.empty()) return eval(pos);
+
+  for (auto move : all_moves) {
+    auto undo_info = pos.do_move(move);
+    best = std::max(-negamax(pos, depth - 1), best);
+    pos.undo_move(undo_info);
+  }
+
+  return best;
+}
+
+Score Engine::eval(const Position& pos) const {
   Score score = 0;
 
-  score += material_score();
+  score += material_score(pos);
 
-  return position.get_player() == Color::WHITE ? score : -score;
+  return pos.get_player() == Color::WHITE ? score : -score;
 }
 
-Move Engine::search(uint8_t depth) {
-  Move bestMove = {0, 0};
+Score Engine::material_score(const Position& pos) const {
+  Score score = 0;
 
-  return bestMove;
+  score += 100 * (pos.count_pieces(W_PAWN) - pos.count_pieces(B_PAWN));
+  score += 300 * (pos.count_pieces(W_KNIGHT) - pos.count_pieces(B_KNIGHT));
+  score += 320 * (pos.count_pieces(W_BISHOP) - pos.count_pieces(B_BISHOP));
+  score += 500 * (pos.count_pieces(W_ROOK) - pos.count_pieces(B_ROOK));
+  score += 900 * (pos.count_pieces(W_QUEEN) - pos.count_pieces(B_QUEEN));
+  return score;
 }
+
 
 Score Engine::material_score() {
-  Score score = 0;
-
-  score += 100 * (position.count_pieces(W_PAWN) - position.count_pieces(B_PAWN));
-  score += 300 * (position.count_pieces(W_KNIGHT) - position.count_pieces(B_KNIGHT));
-  score += 320 * (position.count_pieces(W_BISHOP) - position.count_pieces(B_BISHOP));
-  score += 500 * (position.count_pieces(W_ROOK) - position.count_pieces(B_ROOK));
-  score += 900 * (position.count_pieces(W_QUEEN) - position.count_pieces(B_QUEEN));
-  return score;
+  return material_score(position);
 }
 
 Engine::~Engine() {
--- src/engine.h
+++ src/engine.h
@@ -1,9 +1,7 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-#include "movegen.h"
 #include "position.h"
-#include "utils.h"
 
 #define PROMETHEUS_VERSION "0.1"
 
@@ -13,12 +11,18 @@ public:
   ~Engine();
 
   Score eval();
-  Move search(uint8_t depth);
-  void set_position(const Position& pos) { position = pos; }
+  Move ponder();
+  void set_position(const Position &pos);
+
+  static const Score INF = 1e9;
+  static const unsigned int SEARCH_DEPTH = 4;
 
 private:
   Position position;
 
+  Score negamax(Position pos, uint8_t depth);
+  Score eval(const Position& pos) const;
+  Score material_score(const Position& pos) const;
   Score material_score();
 };
 
